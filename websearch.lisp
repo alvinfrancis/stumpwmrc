81634dc19248af2ca0438fd122e66d49695c76ee
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-03-05 23:04:16 +0800

    Coding style

--- websearch.lisp
+++ websearch.lisp
@@ -1,4 +1,4 @@
-(defmacro make-web-jump (name url-prefix &optional (doc nil doc-supplied-p))
+(defmacro def-web-jump (name url-prefix &optional (doc nil doc-supplied-p))
   `(defcommand ,name (search)
        ((:rest ,(concatenate 'string (symbol-name name) ": ")))
      ,(if doc-supplied-p
@@ -7,12 +7,12 @@
      (run-shell-command (format nil "firefox '~A'"
                                 (concat ,url-prefix (substitute #\+ #\Space search))))))
 
-(make-web-jump google    "http://www.google.com/search?q=")
-(make-web-jump wikipedia "http://en.wikipedia.org/wiki/Special:Search?fulltext=Search&search=")
-(make-web-jump youtube   "http://youtube.com/results?search_query=")
-(make-web-jump arch      "https://wiki.archlinux.org/index.php?title=Special%%3ASearch&search="
-               "Search the Arch Linux Wiki")
-(make-web-jump pac       "http://www.archlinux.org/packages/?q="
-               "Search pacman packages")
-(make-web-jump aur       "http://aur.archlinux.org/packages.php?K="
-               "Search AUR packages")
+(def-web-jump google "http://www.google.com/search?q=")
+(def-web-jump wikipedia "http://en.wikipedia.org/wiki/Special:Search?fulltext=Search&search=")
+(def-web-jump youtube "http://youtube.com/results?search_query=")
+(def-web-jump arch "https://wiki.archlinux.org/index.php?title=Special%%3ASearch&search="
+  "Search the Arch Linux Wiki")
+(def-web-jump pac "http://www.archlinux.org/packages/?q="
+  "Search pacman packages")
+(def-web-jump aur "http://aur.archlinux.org/packages.php?K="
+  "Search AUR packages")
